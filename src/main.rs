1ea3220cb5a63c47354ac6dc18b67afd0a7e1f83
Shane McDonough <shanemcdman@gmail.com>
2021-07-16 20:00:58 -0700

    Add stack.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 mod binary_tree;
 use binary_tree::{make_tree_vals, Tree};
 
+mod stack;
+use stack::Stack;
+
 fn print_heading(title: &str) {
     let sep = "-".repeat(30);
     println!("{} {} {}", sep, title, sep);
 }
 
 fn main() {
-    //Binary Tree
+    // Binary Tree
     print_heading("Binary Tree");
     let t = Tree::from_vec(make_tree_vals(50));
     t.print_val();
@@ -15,4 +18,26 @@ fn main() {
     t.print_fs();
     println!("{}", t.contains(&5));
     println!("{:?}", make_tree_vals(10));
+    // Stack
+    print_heading("Stack");
+    let mut s = Stack::<i32>::new();
+    for i in 0..15 {
+        s.push(i);
+    }
+    for _i in 0..16 {
+        if let Some(val) = s.pop() {
+            println!("{}", val);
+        } else {
+            println!("Empty!");
+        }
+    }
+    s = Stack::from_vec(vec![1, 2, 3, 4]);
+    for _i in 0..5 {
+        if let Some(val) = s.pop() {
+            println!("{}", val);
+        } else {
+            println!("Empty!");
+        }
+    }
+    println!("{}, {}", s.is_empty(), Stack::<String>::new().is_empty());
 }
---
+++ src/stack.rs
@@ -0,0 +1,28 @@
+pub struct Stack<T> {
+    vals: Vec<T>,
+}
+
+impl<T> Stack<T> {
+    pub fn new() -> Self {
+        Self {
+            vals: vec![],
+        }
+    }
+    pub fn from_vec(vec: Vec<T>) -> Self{
+        Self {
+            vals: vec,
+        }
+    }
+
+    pub fn push(&mut self, val: T) {
+        self.vals.push(val)
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        self.vals.pop()
+    }
+    
+    pub fn is_empty(&self) -> bool {
+        self.vals.is_empty()
+    }
+}
